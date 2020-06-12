b9a411c13bec0feb4469c6c2bdd07649495581f8
Batur Kacamak <batisonline@gmail.com>
2020-06-12 08:40:30 +0200

    better directory location

--- bootstrap.php
+++ bootstrap.php
@@ -9,10 +9,10 @@
 */
 
 use Rambouillet\Rambouillet;
-
-require_once dirname(__FILE__) . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 defined('RAMBUILLET_FILE') or define(
     'RAMBUILLET_FILE',
     str_replace(dirname(__FILE__, 2) . '/', '', __FILE__)
 );
+
 Rambouillet::getInstance()->init();
