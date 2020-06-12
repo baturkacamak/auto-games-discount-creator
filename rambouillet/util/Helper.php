8e978655d4708a6cae1a91bef475057eb319c7fe
Batur Kacamak <batisonline@gmail.com>
2020-06-12 08:49:22 +0200

    code refactor

--- rambouillet/util/Helper.php
+++ rambouillet/util/Helper.php
@@ -2,7 +2,10 @@
 
 namespace Rambouillet\Util;
 
-if (! class_exists('Rambouillet\Util\Helper')) {
+use DOMXPath;
+use GuzzleHttp\Client;
+
+if (!class_exists('Rambouillet\Util\Helper')) {
     /**
      * Class Helper
      *
@@ -19,45 +22,45 @@ if (! class_exists('Rambouillet\Util\Helper')) {
          */
         public static function getTurkishDate($f, $zt = 'now')
         {
-            $z        = date("$f", strtotime($zt));
+            $z = date("$f", strtotime($zt));
             $donustur = [
-                'Monday'    => 'Pazartesi',
-                'Tuesday'   => 'Salı',
+                'Monday' => 'Pazartesi',
+                'Tuesday' => 'Salı',
                 'Wednesday' => 'Çarşamba',
-                'Thursday'  => 'Perşembe',
-                'Friday'    => 'Cuma',
-                'Saturday'  => 'Cumartesi',
-                'Sunday'    => 'Pazar',
-                'January'   => 'Ocak',
-                'February'  => 'Şubat',
-                'March'     => 'Mart',
-                'April'     => 'Nisan',
-                'May'       => 'Mayıs',
-                'June'      => 'Haziran',
-                'July'      => 'Temmuz',
-                'August'    => 'Ağustos',
+                'Thursday' => 'Perşembe',
+                'Friday' => 'Cuma',
+                'Saturday' => 'Cumartesi',
+                'Sunday' => 'Pazar',
+                'January' => 'Ocak',
+                'February' => 'Şubat',
+                'March' => 'Mart',
+                'April' => 'Nisan',
+                'May' => 'Mayıs',
+                'June' => 'Haziran',
+                'July' => 'Temmuz',
+                'August' => 'Ağustos',
                 'September' => 'Eylül',
-                'October'   => 'Ekim',
-                'November'  => 'Kasım',
-                'December'  => 'Aralık',
-                'Mon'       => 'Pts',
-                'Tue'       => 'Sal',
-                'Wed'       => 'Çar',
-                'Thu'       => 'Per',
-                'Fri'       => 'Cum',
-                'Sat'       => 'Cts',
-                'Sun'       => 'Paz',
-                'Jan'       => 'Oca',
-                'Feb'       => 'Şub',
-                'Mar'       => 'Mar',
-                'Apr'       => 'Nis',
-                'Jun'       => 'Haz',
-                'Jul'       => 'Tem',
-                'Aug'       => 'Ağu',
-                'Sep'       => 'Eyl',
-                'Oct'       => 'Eki',
-                'Nov'       => 'Kas',
-                'Dec'       => 'Ara',
+                'October' => 'Ekim',
+                'November' => 'Kasım',
+                'December' => 'Aralık',
+                'Mon' => 'Pts',
+                'Tue' => 'Sal',
+                'Wed' => 'Çar',
+                'Thu' => 'Per',
+                'Fri' => 'Cum',
+                'Sat' => 'Cts',
+                'Sun' => 'Paz',
+                'Jan' => 'Oca',
+                'Feb' => 'Şub',
+                'Mar' => 'Mar',
+                'Apr' => 'Nis',
+                'Jun' => 'Haz',
+                'Jul' => 'Tem',
+                'Aug' => 'Ağu',
+                'Sep' => 'Eyl',
+                'Oct' => 'Eki',
+                'Nov' => 'Kas',
+                'Dec' => 'Ara',
             ];
             foreach ($donustur as $en => $tr) {
                 $z = str_replace($en, $tr, $z);
