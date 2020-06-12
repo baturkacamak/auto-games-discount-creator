f409b5938aceead5f2d06a5acc1a2ad0d322facc
Batur Kacamak <batisonline@gmail.com>
2020-06-12 08:46:32 +0200

    removed cookie which is unnecessary to query the base site added query_type to decide whether to query hourly or daily for now

--- rambouillet/unyson/PluginSettings.php
+++ rambouillet/unyson/PluginSettings.php
@@ -2,144 +2,155 @@
 
 namespace Rambouillet\Unyson;
 
-/**
- * Used in fw()->backend
- *
- * @internal
- */
-class PluginSettings extends \FW_Settings_Form
-{
-    /**
-     * @return array
-     */
-    public function get_options()
-    {
-        return [
-            'box_settings' => [
-                'type' => 'box',
-                'title' => esc_html__('Rambouillet Plugin Settings', 'mariselle'),
-                'options' => [
-                    'itad_cookie' => ['type' => 'textarea'],
-                    'queries' => [
-                        'type' => 'addable-box',
-                        'template' => '{{- query_key }}', // box title
-                        'box-options' => [
-                            'query_key' => [
-                                'type' => 'text',
-                                'attr' => ['autocomplete' => 'off'],
-                            ],
-                            'query_value' => [
-                                'type' => 'text',
-                                'attr' => ['autocomplete' => 'off'],
-                            ],
-                        ],
-                    ],
-                    'tags' => ['type' => 'textarea'],
-                ],
-            ],
-        ];
-    }
+use FW_Settings_Form;
 
+if (!class_exists('Rambouillet\Unyson\PluginSettings')) {
     /**
-     * @param array|callable $values
+     * Used in fw()->backend
      *
-     * @return $this|\FW_Settings_Form
-     */
-    public function set_values($values)
-    {
-        \FW_WP_Option::set('rambouillet:rambouillet', null, $values);
-
-        return $this;
-    }
-
-    /**
-     * @param null $key
-     *
-     * @return array|mixed|null
-     */
-    public function get_values($key = null)
-    {
-        return \FW_WP_Option::get(
-            'rambouillet:rambouillet',
-            $key,
-            []
-        );
-    }
-
-    /**
      * @internal
      */
-    public function _action_admin_menu()
+    class PluginSettings extends FW_Settings_Form
     {
-        $data = [
-            'capability' => 'manage_options',
-            'slug' => fw()->backend->_get_settings_page_slug(),
-            'content_callback' => [$this, 'render'],
-        ];
-
-        if (!current_user_can($data['capability'])) {
-            return;
-        }
-
-        if (fw()->theme->get_config('disable_theme_settings_page', false)) {
-            return;
+        /**
+         * @return array
+         */
+        public function get_options()
+        {
+            return [
+                'box_settings' => [
+                    'type' => 'box',
+                    'title' => esc_html__('Rambouillet Plugin Settings', 'mariselle'),
+                    'options' => [
+                        'queries' => [
+                            'type' => 'addable-box',
+                            'template' => '{{- query_key }}', // box title
+                            'box-options' => [
+                                'query_key' => [
+                                    'type' => 'text',
+                                    'attr' => ['autocomplete' => 'off'],
+                                ],
+                                'query_value' => [
+                                    'type' => 'text',
+                                    'attr' => ['autocomplete' => 'off'],
+                                ],
+                                'query_type' => [
+                                    'value' => 'daily',
+                                    'type' => 'select',
+                                    'choices' => [
+                                        'daily' => 'Daily',
+                                        'hourly' => 'Hourly'
+                                    ]
+                                ],
+                            ],
+                        ],
+                        'tags' => ['type' => 'textarea'],
+                    ],
+                ],
+            ];
         }
 
         /**
-         * Collect $hookname that contains $data['slug'] before the action
-         * and skip them in verification after action
-         */ {
-        global $_registered_pages;
+         * @param array|callable $values
+         *
+         * @return $this|FW_Settings_Form
+         */
+        public function set_values($values)
+        {
+            \FW_WP_Option::set('rambouillet:rambouillet', null, $values);
 
-        $found_hooknames = [];
-
-        if (!empty($_registered_pages)) {
-            foreach ($_registered_pages as $hookname => $b) {
-                if (strpos($hookname, $data['slug']) !== false) {
-                    $found_hooknames[$hookname] = true;
-                }
-            }
+            return $this;
         }
-    }
 
         /**
-         * Check if settings menu was added in the action above
-         */ {
-        $menu_exists = false;
+         * @param null $key
+         *
+         * @return array|mixed|null
+         */
+        public function get_values($key = null)
+        {
+            return \FW_WP_Option::get(
+                'rambouillet:rambouillet',
+                $key,
+                []
+            );
+        }
 
-        if (!empty($_registered_pages)) {
-            foreach ($_registered_pages as $hookname => $b) {
-                if (isset($found_hooknames[$hookname])) {
-                    continue;
-                }
+        /**
+         * @internal
+         */
+        public function _action_admin_menu()
+        {
+            $data = [
+                'capability' => 'manage_options',
+                'slug' => fw()->backend->_get_settings_page_slug(),
+                'content_callback' => [$this, 'render'],
+            ];
 
-                if (strpos($hookname, $data['slug']) !== false) {
-                    $menu_exists = true;
-                    break;
+            if (!current_user_can($data['capability'])) {
+                return;
+            }
+
+            if (fw()->theme->get_config('disable_theme_settings_page', false)) {
+                return;
+            }
+
+            /**
+             * Collect $hookname that contains $data['slug'] before the action
+             * and skip them in verification after action
+             */ {
+            global $_registered_pages;
+
+            $found_hooknames = [];
+
+            if (!empty($_registered_pages)) {
+                foreach ($_registered_pages as $hookname => $b) {
+                    if (strpos($hookname, $data['slug']) !== false) {
+                        $found_hooknames[$hookname] = true;
+                    }
                 }
             }
         }
-    }
 
-        if ($menu_exists) {
-            return;
+            /**
+             * Check if settings menu was added in the action above
+             */ {
+            $menu_exists = false;
+
+            if (!empty($_registered_pages)) {
+                foreach ($_registered_pages as $hookname => $b) {
+                    if (isset($found_hooknames[$hookname])) {
+                        continue;
+                    }
+
+                    if (strpos($hookname, $data['slug']) !== false) {
+                        $menu_exists = true;
+                        break;
+                    }
+                }
+            }
         }
 
-        add_theme_page(
-            __('Rambouillet Settings', 'fw'),
-            __('Rambouillet Settings', 'fw'),
-            $data['capability'],
-            'rambouillet-settings',
-            $data['content_callback']
-        );
-    }
+            if ($menu_exists) {
+                return;
+            }
+
+            add_theme_page(
+                __('Rambouillet Settings', 'fw'),
+                __('Rambouillet Settings', 'fw'),
+                $data['capability'],
+                'rambouillet-settings',
+                $data['content_callback']
+            );
+        }
 
 
-    /**
-     *
-     */
-    protected function _init()
-    {
-        add_action('admin_menu', [$this, '_action_admin_menu']);
+        /**
+         *
+         */
+        protected function _init()
+        {
+            add_action('admin_menu', [$this, '_action_admin_menu']);
+        }
     }
 }
