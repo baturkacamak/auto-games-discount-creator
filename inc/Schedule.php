ef14fb6ec0b3edfad22432b4b77cce69969b50e9
Batur Kacamak <batisonline@gmail.com>
2023-01-29 19:24:23 +0100

    Update Schedule.php

--- inc/Schedule.php
+++ inc/Schedule.php
@@ -4,55 +4,54 @@ namespace Rambouillet;
 
 use Curl\Curl;
 use Rambouillet\Unyson\PluginSettings;
+use Rambouillet\Utility\Medoo;
 
 if (!class_exists('Rambouillet\Schedule')) {
-    /**
-     * Class Schedule
-     *
-     * @package Rambouillet
-     */
-    class Schedule extends AbstractRambouillet
-    {
-        /**
-         *
-         * @throws \GuzzleHttp\Exception\GuzzleException
-         * @throws \Exception
-         */
-        public static function startDailyPost()
-        {
-            $scrape = new Scraper('daily', Rambouillet::getInstance()->settings);
-            $results = $scrape->getQueryResults();
-            $parser = new Parser();
-            $parser->setQueryResults($results);
-            $parser->parseQueryResults();
-            new Poster();
-        }
+	/**
+	 * Class Schedule
+	 *
+	 * @package Rambouillet
+	 */
+	class Schedule extends AbstractRambouillet
+	{
+		/**
+		 *
+		 * @throws \GuzzleHttp\Exception\GuzzleException
+		 * @throws \Exception
+		 */
+		public static function startDailyPost()
+		{
+			$scrape  = new Scraper('daily', Rambouillet::getInstance()->settings);
+			$results = $scrape->getQueryResults();
+			$parser  = new Parser($results);
+			$parser->parseQueryResults();
+			new Poster();
+		}
 
-        /**
-         *
-         * @throws \GuzzleHttp\Exception\GuzzleException
-         * @throws \Exception
-         */
-        public static function startHourlyPost()
-        {
-            $scrape = new Scraper('hourly', Rambouillet::getInstance()->settings);
-            $results = $scrape->getQueryResults();
-            $parser = new Parser();
-            $parser->setQueryResults($results);
-            $parser->parseQueryResults();
+		/**
+		 *
+		 * @throws \GuzzleHttp\Exception\GuzzleException
+		 * @throws \Exception
+		 */
+		public static function startHourlyPost()
+		{
+			$scrape  = new Scraper('hourly', Rambouillet::getInstance()->settings);
+			$results = $scrape->getQueryResults();
+			$parser  = new Parser($results);
+			$parser->parseQueryResults();
+			new Poster('hourly');
+		}
 
-            new Poster('hourly');
-        }
 
-        public function addActions()
-        {
-            if (!wp_next_scheduled('startScheduleDailyPost')) {
-                wp_schedule_event(strtotime('06:00:00'), 'daily', 'startScheduleDailyPost');
-            }
+		public function addActions()
+		{
+			if (!wp_next_scheduled('startScheduleDailyPost')) {
+				wp_schedule_event(strtotime('06:00:00'), 'daily', 'startScheduleDailyPost');
+			}
 
-            if (!wp_next_scheduled('startScheduleHourlyPost')) {
-                wp_schedule_event(time(), 'hourly', 'startScheduleHourlyPost');
-            }
-        }
-    }
+			if (!wp_next_scheduled('startScheduleHourlyPost')) {
+				wp_schedule_event(time(), 'hourly', 'startScheduleHourlyPost');
+			}
+		}
+	}
 }
