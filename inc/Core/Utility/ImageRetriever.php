1437360c5b76d74fa16f14ad716607bc09cf1cee
Batur Kacamak <batisonline@gmail.com>
2023-02-15 06:40:49 +0100

    Update and rename Web.php to ImageRetriever.php

--- inc/Core/Utility/ImageRetriever.php
+++ inc/Core/Utility/ImageRetriever.php
@@ -5,13 +5,13 @@ namespace AutoGamesDiscountCreator\Core\Utility;
 use AutoGamesDiscountCreator\DOMNodeHandler;
 
 /**
- * Class Web
+ * Class ImageRetriever
  *
- * This class is responsible for making web requests and parsing the responses.
+ * This class is responsible for retrieving the remote images from a given URL.
  *
  * @package AutoGamesDiscountCreator\Core\Utility
  */
-class Web
+class ImageRetriever
 {
 	/**
 	 * @var WebClient
@@ -24,7 +24,7 @@ class Web
 	private $domHandler;
 
 	/**
-	 * Web constructor.
+	 * ImageRetriever constructor.
 	 *
 	 * Initializes the WebClient and DOMNodeHandler instances.
 	 */
@@ -35,19 +35,28 @@ class Web
 	}
 
 	/**
-	 * Gets the remote image from the given URL.
+	 * Retrieves the remote image from the given URL.
 	 *
-	 * @param string $url The URL to make the request to.
+	 * @param string $url The URL to retrieve the remote image from.
 	 *
 	 * @return string The URL of the remote image.
 	 */
-	public function getRemoteImage(string $url): string
+	public function retrieve(string $url): string
 	{
+		// Get the HTML response from the URL
 		$html_response = $this->webClient->get($url);
+
+		// If the HTML response was successful
 		if ($html_response) {
+			// Create the XPath object from the HTML response
 			$xpath           = $this->domHandler->createXpathFromHtml($html_response);
+
+			// Get the meta image node from the XPath object
 			$meta_image_node = $xpath->query('//meta[@property="og:image"]');
+
+			// If the meta image node exists
 			if ($meta_image_node && $meta_image_node->length) {
+				// Return the content attribute of the meta image node
 				return $meta_image_node->item(0)->getAttribute('content') ? $meta_image_node->item(0)->getAttribute(
 					'content'
 				) : '';
