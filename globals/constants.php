ce0016f39d07211a59ffc97528aefbb6d152792d
baturkacamak <batisonline@gmail.com>
2023-02-19 18:20:46 +0100

    fixed constants to work from base directory

--- globals/constants.php
+++ globals/constants.php
@@ -6,14 +6,14 @@
  * Time: 22:35
  */
 
-define('AGDC_VERSION', '0.1.0');
-define('AGDC_PLUGIN_FILE', __FILE__);
-define('AGDC_PLUGIN_DIR', __DIR__);
+const AGDC_VERSION = '0.1.0';
+define('AGDC_PLUGIN_DIR', dirname(__DIR__));
+const AGDC_PLUGIN_FILE = AGDC_PLUGIN_DIR . '/autogamesdiscountcreator.php';
 if (function_exists('plugin_dir_url')) {
 	define('AGDC_PLUGIN_URL', plugin_dir_url(__FILE__));
 }
 
 defined('AGDC_BASE_FILE') or define(
 	'AGDC_BASE_FILE',
-	str_replace(dirname(__FILE__, 2) . '/', '', __FILE__)
+	str_replace(dirname(AGDC_PLUGIN_FILE, 2) . '/', '', AGDC_PLUGIN_FILE)
 );
